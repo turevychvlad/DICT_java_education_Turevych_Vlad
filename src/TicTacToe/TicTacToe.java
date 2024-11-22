da7b6d4ae30c638c372bc75d178fa1a6bcaf84ef
turevychvlad <v.o.turevych@student.khai.edu>
2024-11-22 18:35:20 +0300

    4-st stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -5,76 +5,37 @@ import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Запрос строки для состояния поля
-        System.out.print("Enter cells (9 characters, only 'X', 'O', '_'): ");
-        String input = scanner.nextLine();
-
-        // Проверка ввода
-        while (!isValidInput(input)) {
-            System.out.print("Invalid input. Enter exactly 9 characters ('X', 'O', '_'): ");
-            input = scanner.nextLine();
-        }
-
-        // Создание игрового поля
+        String input = "_XXOO_OX_";
         char[][] board = {
                 {input.charAt(0), input.charAt(1), input.charAt(2)},
                 {input.charAt(3), input.charAt(4), input.charAt(5)},
                 {input.charAt(6), input.charAt(7), input.charAt(8)}
         };
 
-        // Печать игрового поля
         printBoard(board);
 
-        // Анализ состояния игры
-        System.out.println(checkGameState(board));
-    }
+        while (true) {
+            System.out.print("Enter the coordinates: ");
+            try {
+                int row = scanner.nextInt() - 1;
+                int col = scanner.nextInt() - 1;
 
-    // Метод для проверки валидности ввода
-    public static boolean isValidInput(String input) {
-        if (input.length() != 9) return false;
-        for (char c : input.toCharArray()) {
-            if (c != 'X' && c != 'O' && c != '_') return false;
-        }
-        return true;
-    }
-
-    // Метод для анализа состояния игры
-    public static String checkGameState(char[][] board) {
-        int xCount = 0, oCount = 0;
-
-        // Подсчет количества X и O
-        for (char[] row : board) {
-            for (char cell : row) {
-                if (cell == 'X') xCount++;
-                if (cell == 'O') oCount++;
+                if (row < 0 || row > 2 || col < 0 || col > 2) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                } else if (board[row][col] != '_') {
+                    System.out.println("This cell is occupied! Choose another one!");
+                } else {
+                    board[row][col] = 'X';
+                    printBoard(board);
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!");
+                scanner.nextLine(); // Очистка ввода
             }
         }
-
-        boolean xWins = hasWinner(board, 'X');
-        boolean oWins = hasWinner(board, 'O');
-
-        // Логика определения состояния игры
-        if (xWins && oWins || Math.abs(xCount - oCount) > 1) return "Impossible";
-        if (xWins) return "X wins";
-        if (oWins) return "O wins";
-        if (xCount + oCount == 9) return "Draw";
-        return "Game not finished";
     }
 
-    // Метод для проверки победителя
-    private static boolean hasWinner(char[][] board, char player) {
-        for (int i = 0; i < 3; i++) {
-            // Проверка строк и столбцов
-            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) return true;
-            if (board[0][i] == player && board[1][i] == player && board[2][i] == player) return true;
-        }
-        // Проверка диагоналей
-        return board[0][0] == player && board[1][1] == player && board[2][2] == player ||
-                board[0][2] == player && board[1][1] == player && board[2][0] == player;
-    }
-
-    // Метод для печати игрового поля
     public static void printBoard(char[][] board) {
         System.out.println("---------");
         for (char[] row : board) {
