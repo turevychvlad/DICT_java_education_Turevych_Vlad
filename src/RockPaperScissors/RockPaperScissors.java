c72b2c7b161b5592b049bfbe217c821b784c1b26
turevychvlad <v.o.turevych@student.khai.edu>
2024-11-22 19:48:36 +0300

    2-st stage

--- src/RockPaperScissors/RockPaperScissors.java
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -1,23 +1,31 @@
 package RockPaperScissors;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your move (rock, paper, scissors):");
+        Random random = new Random();
+        String[] options = {"rock", "paper", "scissors"};
 
+        System.out.println("Enter your move (rock, paper, scissors):");
         while (true) {
             String userMove = scanner.nextLine();
-
-            if (userMove.equalsIgnoreCase("rock")) {
-                System.out.println("Sorry, but the computer chose paper");
-            } else if (userMove.equalsIgnoreCase("paper")) {
-                System.out.println("Sorry, but the computer chose scissors");
-            } else if (userMove.equalsIgnoreCase("scissors")) {
-                System.out.println("Sorry, but the computer chose rock");
-            } else {
+            if (!userMove.equals("rock") && !userMove.equals("paper") && !userMove.equals("scissors")) {
                 System.out.println("Invalid input. Please enter rock, paper, or scissors.");
+                continue;
+            }
+
+            String computerMove = options[random.nextInt(3)];
+            if (userMove.equals(computerMove)) {
+                System.out.println("There is a draw (" + computerMove + ")");
+            } else if ((userMove.equals("rock") && computerMove.equals("scissors")) ||
+                    (userMove.equals("scissors") && computerMove.equals("paper")) ||
+                    (userMove.equals("paper") && computerMove.equals("rock"))) {
+                System.out.println("Well done. The computer chose " + computerMove + " and failed");
+            } else {
+                System.out.println("Sorry, but the computer chose " + computerMove);
             }
         }
     }
