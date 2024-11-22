dfa263b83844442e7444500d9a293804d8100a66
turevychvlad <v.o.turevych@student.khai.edu>
2024-11-22 17:59:50 +0300

    Rebase succesfull

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("education");
+        System.out.println("Java education");
         //comment
     }
 }
